833e06caec415485a9107963c526c57c9798f04c
Muhamad Adib Minanurohman <inanadib085@gmail.com>
2023-12-02 14:36:41 +0700

    Fix : navigate EJurnal to UnggahEJurnal

--- app/src/main/java/com/massive_project_eduvion_dosen/activity/EJurnalActivity.kt
+++ app/src/main/java/com/massive_project_eduvion_dosen/activity/EJurnalActivity.kt
@@ -2,11 +2,20 @@ package com.massive_project_eduvion_dosen.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.content.Intent
+import android.widget.ImageButton
 import com.massive_project_eduvion_dosen.R
 
 class EJurnalActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_ejurnal)
+
+        val buttonUnggah: ImageButton = findViewById(R.id.bt_unggah)
+
+        buttonUnggah.setOnClickListener {
+            val intent = Intent(this, UnggahEJurnalActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
