85cd1f0f629c09d47659b6833e35cec2c16bcd21
Muhamad Adib Minanurohman <inanadib085@gmail.com>
2023-12-19 23:05:20 +0700

    feat : fragment konsultasi

--- app/src/main/java/com/massive_project_eduvion_dosen/network/ApiClient.kt
+++ app/src/main/java/com/massive_project_eduvion_dosen/network/ApiClient.kt
@@ -5,7 +5,7 @@ import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 object RetrofitClient {
-    private const val BASE_URL = "http://192.168.1.6:8080/api/"
+    private const val BASE_URL = "http://192.168.1.9:8080/api/"
     private  lateinit var apiService: ApiService
 
     fun getApi(context: Context): ApiService{
